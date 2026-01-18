32314400690ead62a4ce9a22f9f19deb8c89678c
Dr. Mary Lebens <mary@marylebens.com>
2026-01-18 23:20:40 +0000

    Created calculator program

---
+++ Calculator.py
@@ -0,0 +1,10 @@
+# Calculator program - Adds two numbers together
+print("Simple Calculator")
+print("Enter first number:")
+num1 = float(input())
+
+print("Enter second number:")
+num2 = float(input())
+
+sum_value = num1 + num2
+print("The sum is:", sum_value)
